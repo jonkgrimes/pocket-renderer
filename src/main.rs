94259319e85fb2ec41eecf3ac42dc5e1d2fda083
Jon Grimes <jong@spiceworks.com>
2017-06-11 10:18:11 -0500

    What I've got so far; currently doesn't compile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate image;
+
+use std::fs::File;
+use std::path::Path;
+use std::vec::Vec;
+use image::{ImageBuffer, Pixel};
+
+const WHITE: [u8; 3] = [255 as u8, 0 as u8, 0 as u8];
+
+fn line<P: Pixel + 'static>(imgbuf: &mut ImageBuffer<P, Vec<P::Subpixel>>) {
+    imgbuf.put_pixel(45, 67, image::Rgb(WHITE));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut imgbuf = ImageBuffer::new(100, 100);
+    line(&mut imgbuf);
+    let ref mut fout = File::create(&Path::new("image.png")).unwrap();
+    let _ = image::ImageRgb8(imgbuf).save(fout, image::PNG);
 }
