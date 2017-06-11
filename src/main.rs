7b2dc087d03bbe89a7e6eb8f30f4f2064e49c8e3
Jon Grimes <jong@spiceworks.com>
2017-06-11 12:10:16 -0500

    First swipe at the line drawing alogrith; lots of type coercions

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,21 @@ use image::{ImageBuffer, Pixel};
 
 const WHITE: [u8; 3] = [255 as u8, 255 as u8, 255 as u8];
 
-fn line<P: Pixel + 'static>(imgbuf: &mut ImageBuffer<P, Vec<P::Subpixel>>, pixel: P) {
-    imgbuf.put_pixel(45, 67, pixel);
+fn line<P: Pixel + 'static>(x0: u32, y0: u32, x1: u32, y1: u32, imgbuf: &mut ImageBuffer<P, Vec<P::Subpixel>>, pixel: P) {
+    let mut t = 0.0; 
+    while t < 1.0 {
+        let x = x0 as f32 * (1.0 - t) + x1 as f32 * t;
+        println!("{}", x);
+        let y = y0 as f32 * (1.0 - t) + y1  as f32 * t;
+        println!("{}", y);
+        imgbuf.put_pixel(x as u32, y as u32, pixel);
+        t += 0.01;
+    }
 }
 
 fn main() {
     let mut imgbuf = ImageBuffer::new(100, 100);
-    line(&mut imgbuf, image::Rgb(WHITE));
+    line(24, 56, 78, 45, &mut imgbuf, image::Rgb(WHITE));
     let ref mut fout = File::create(&Path::new("rendered.png")).unwrap();
     let _ = image::ImageRgb8(imgbuf).save(fout, image::PNG);
 }
