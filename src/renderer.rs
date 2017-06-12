dcfdd4ce00bb8b01fe8d10a5533fd1b57d391774
Jon Grimes <jong@spiceworks.com>
2017-06-12 08:41:51 -0500

    Playing with speeding this up

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,11 +1,28 @@
 use image::{ImageBuffer, Pixel};
+use std::mem;
 
 pub fn line<P: Pixel + 'static>(x0: u32, y0: u32, x1: u32, y1: u32, imgbuf: &mut ImageBuffer<P, Vec<P::Subpixel>>, pixel: P) {
     let mut t = 0.0; 
-    while t < 1.0 {
-        let x = x0 as f32 * (1.0 - t) + x1 as f32 * t;
-        let y = y0 as f32 * (1.0 - t) + y1  as f32 * t;
-        imgbuf.put_pixel(x as u32, y as u32, pixel);
-        t += 0.01;
+    let mut steep = false;
+
+    if ((x0-x1).abs() < (y0-y1).abs()) { // if line is steep, transpose the image
+        mem::swap(x0, y0);
+        mem::swap(x1, y1);
+        steep = true;
+    }
+
+    if (x0 > x1) {
+        mem::swap(x0, x1);
+        mem::swap(y0, y1);
+    }
+
+    for x in (x0..x1) {
+        let t = (x - x0) as f32/(x1 - x0) as f32;
+        let y = y0 * (1.0 - t) + y1 as f32 * t;
+        if (steep) {
+            imgbuf.put_pixel(y as u32, x as u32, pixel);
+        } else {
+            imgbuf.put_pixel(x as u32, y as u32, pixel);
+        }
     }
 }
