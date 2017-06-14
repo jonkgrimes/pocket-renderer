52657e005f59d345e11eb9794e1fbae9686b6db7
Jon Grimes <jong@spiceworks.com>
2017-06-14 07:45:55 -0500

    Figure it out; algorithm was wrong

--- src/renderer.rs
+++ src/renderer.rs
@@ -7,7 +7,6 @@ struct Point {
 }
 
 pub fn line<P: Pixel + 'static>(x0: i32, y0: i32, x1: i32, y1: i32, imgbuf: &mut ImageBuffer<P, Vec<P::Subpixel>>, pixel: P) {
-    let mut t = 0.0; 
     let mut steep = false;
     let mut point0 = Point { x: x0, y: y0 };
     let mut point1 = Point { x: x1, y: y1 };
@@ -31,16 +30,18 @@ pub fn line<P: Pixel + 'static>(x0: i32, y0: i32, x1: i32, y1: i32, imgbuf: &mut
 
     println!("Before drawing: {:?}, {:?}", point0, point1);
 
-    let mut x = 0;
+    let mut x = point0.x;
     while x <= point1.x {
         let t = (x - point0.x) as f32/(point1.x - point0.x) as f32;
-        let y = point0.y as f32 * (1.0 - t) + point1.y as f32 * t;
+        println!("y0 = {}, y1 = {}, t = {}", point0.y, point1.y, t);
+        let y = (point0.y as f32 * (1.0 - t)) + (point1.y as f32 * t);
         println!("x: {}, y: {}", x, y);
         if steep {
             imgbuf.put_pixel(y as u32, x as u32, pixel);
         } else {
             imgbuf.put_pixel(x as u32, y as u32, pixel);
         }
+        println!("Iterating...");
         x += 1;
     }
 }
