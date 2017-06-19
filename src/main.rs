4155a84492e708fdc087b0ada6495c18790c4281
Jon Grimes <jong@spiceworks.com>
2017-06-18 19:35:10 -0500

    I'm able to open and parse the file

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ use std::fs::File;
 use std::path::Path;
 use image::{ImageBuffer};
 
+use model::Model;
+
 const WHITE: [u8; 3] = [255 as u8, 255 as u8, 255 as u8];
 
 pub mod model;
 pub mod geometry;
 pub mod renderer;
 
-
 fn main() {
     let mut imgbuf = ImageBuffer::new(100, 100);
     renderer::line(20, 13, 40, 80, &mut imgbuf, image::Rgb(WHITE));
-    let v0 = geometry::Vertex3 { x: 1.0, y: 1.0, z: 1.0 };
-    println!("{:?}", v0);
+    let model = Model::new("models/african_head.obj");
     let ref mut fout = File::create(&Path::new("rendered.png")).unwrap();
     let _ = image::ImageRgb8(imgbuf).save(fout, image::PNG);
 }
--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,8 @@
-extern crate csv;
-
+use std::path::Path;
 use std::fs::File;
-use 
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::vec::Vec;
 
 use geometry::Vertex3;
 
@@ -11,8 +12,18 @@ pub struct Model {
 
 impl Model {
 
-    pub fn from_file<P: AsRef<Path>>(&self, path: P) -> Model {
-        
+    pub fn new<P: AsRef<Path>>(path: P) -> Model {
+        let verts: Vec<Vertex3<u32>> = Vec::new();;
+        let file = File::open(path);
+        let mut buf_reader = BufReader::new(file.unwrap());
+        for line in buf_reader.lines() {
+            let decoded_line = line.unwrap();
+            let values: Vec<&str> = decoded_line.split(" ").collect();
+            if values[0] == "v" {
+                println!("x = {}, y = {}, z = {}", values[1], values[2], values[3]);
+            }
+        }
+        Model { verts: verts }
     } 
 }
 
