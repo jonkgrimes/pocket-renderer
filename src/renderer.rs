7759c67f7df29fd2d75a52a36442ecc529cab44f
Jon Grimes <jong@spiceworks.com>
2017-08-03 20:54:58 -0500

    Cleaned out that old line drawing function

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,15 +1,8 @@
 extern crate image;
 
 use geometry::{Vertex2, Vertex3, Matrix};
-use image::{DynamicImage, ImageBuffer, RgbImage, Pixel};
-use std::mem;
+use image::{DynamicImage, RgbImage};
 use std::f32;
-
-#[derive(Debug)]
-struct Point {
-    x: i32,
-    y: i32,
-}
 
 pub fn lookat(eye: Vertex3<f32>, center: Vertex3<f32>, up: Vertex3<f32>) -> Matrix {
     let z = (eye - center).normalize();
@@ -100,54 +93,5 @@ pub fn triangle(verts: &[Vertex3<f32>; 3],
                 imgbuf.put_pixel(p.x as u32, p.y as u32, pixel);
             }
         }
-    }
-}
-
-pub fn line<P: Pixel + 'static>(x0: i32,
-                                y0: i32,
-                                x1: i32,
-                                y1: i32,
-                                imgbuf: &mut ImageBuffer<P, Vec<P::Subpixel>>,
-                                pixel: P) {
-    let mut steep = false;
-    let mut point0 = Point { x: x0, y: y0 };
-    let mut point1 = Point { x: x1, y: y1 };
-
-    if (x0 - x1).abs() < (y0 - y1).abs() {
-        // if line is steep, transpose the image
-        point0 = Point { x: y0, y: x0 };
-        point1 = Point { x: y1, y: x1 };
-        steep = true;
-    }
-
-    if point0.x > point1.x {
-        // make it left to right
-        mem::swap(&mut point0, &mut point1);
-    }
-
-    let dx = point1.x - point0.x;
-    let dy = point1.y - point0.y;
-    let derror2 = dy.abs() * 2;
-    let mut error2 = 0;
-
-    let mut x = point0.x;
-    let mut y = point0.y;
-
-    while x <= point1.x {
-        if steep {
-            imgbuf.put_pixel(y as u32, x as u32, pixel);
-        } else {
-            imgbuf.put_pixel(x as u32, y as u32, pixel);
-        }
-        error2 += derror2;
-        if error2 > dx {
-            if point1.y > point0.y {
-                y += 1;
-            } else {
-                y += -1;
-            }
-            error2 -= dx * 2;
-        }
-        x += 1;
     }
 }
